bdd2595197678cf8ebdcf186577681de1fa1b05d
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-02-07 18:37:18 +0700

    feat: Пользовательские hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React, { useMemo, useState } from 'react';
-import Counter from './components/Counter';
-import './syles/App.css'
-import PostItem from './components/PostItem';
+import React, { useState } from 'react';
+import { usePosts } from './hooks/usePosts';
 import PostList from './components/PostList';
 import Mybutton from './components/UI/buttons/Mybutton';
-import MyInput from './components/UI/inputs/MyInput';
 import PostForm from './components/PostForm';
-import Myselect from './components/UI/select/Myselect';
 import PostFilter from './components/PostFilter';
 import Mymodal from './components/UI/MyModal/Mymodal';
+import './syles/App.css'
 
 function App() {
 
-  const [posts, setPosts] = useState(
-    [
-      { id: 1, title: 'aa', body: 'hh' },
-      { id: 2, title: 'vv', body: 'aa' },
-      { id: 3, title: 'kk', body: 'pp' }
-    ]
-  );
-
-  const [filter, setFilter] = useState({ sort: '', query: '' })
-  const [modal, setModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -43,10 +23,9 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-
   return (
     <div className="App">
-      <Mybutton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+      <Mybutton style={{ marginTop: '30px' }} onClick={() => setModal(true)}>
         Создать пост
       </Mybutton>
       <Mymodal visible={modal} setVisible={setModal}>
@@ -59,9 +38,6 @@ function App() {
         setFilter={setFilter}
       />
       <PostList remove={removePost} posts={sortedAndSearchPosts} title='Список постов' />
-
-
-
     </div>
   );
 }
