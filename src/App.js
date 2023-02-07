96958eb7456e45e6e135510acb5d432cf7bbd6f8
Sergey Evdokimov <cadsergrf@yandex.ru>
2023-02-07 09:18:21 +0700

    feat: пост через объект

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import MyInput from './components/UI/inputs/MyInput';
 
 function App() {
 
-  const [posts, setPost] = useState(
+  const [posts, setPosts] = useState(
     [
       { id: 1, title: 'Javascript', body: 'Description' },
       { id: 2, title: 'Python', body: 'Description' },
@@ -16,25 +16,30 @@ function App() {
     ]
   );
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [post, setPost] = useState({title:'', body:''})
 
   const addNewPost = (e) => {
     e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
 
-    setPost([...posts, newPost])
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title:'', body:''})
   }
 
   return (
     <div className="App">
       <form>
-        <MyInput type='text' placeholder='Название поста' value={title} onChange={e => setTitle(e.target.value)} />
-        <MyInput type='text' placeholder='Описание поста' value={body} onChange={e => setBody(e.target.value)} />
+        <MyInput 
+        type='text' 
+        placeholder='Название поста' 
+        value={post.title} 
+        onChange={e => setPost({...post, title: e.target.value})} 
+        />
+        <MyInput 
+        type='text' 
+        placeholder='Описание поста' 
+        value={post.body} 
+        onChange={e => setPost({...post, body: e.target.value})} 
+        />
         <Mybutton onClick={addNewPost}>Создать пост</Mybutton>
       </form>
 
